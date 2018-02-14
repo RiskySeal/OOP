e280e98d8817d933190fb311568f0b69bb21c08a
RiskySeal <Ingemarlilium@gmail.com>
2018-02-14 11:30:33 +0200

    git init

---
+++ index.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ingemar.lilium
+ * Date: 14.02.2018
+ * Time: 9:39
+ */
+
+function test(){
+    $items = [];
+    $firstName ="";
+    foreach ($items as $item){
+        echo "Test";
+    }
+
+    return $firstName;
+
+
+
+}
+
+function test2(){
+
+
+
+
+
+
+}
+
+test2();
+test2();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
